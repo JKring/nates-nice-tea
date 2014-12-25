2d28c4ab731a8cb9d6b447d6f46a62b3db998824
Jake Kring <kringj@gmail.com>
2014-12-25 09:58:01 -0500

    adding lein

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject nates-nice-tea "0.1.0-SNAPSHOT"
   :description "The nicest teas from the nicest guy"
   :url "http://natesnicetea.com"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
